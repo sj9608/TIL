c28a95c64faeb379a098e896ff3fc62e1e8052c1
seungjun <sneak7581@naver.com>
2021-10-25 21:49:11 +0900

    Add : Level2 Joystick
    Programmers Level2 Greedy Algorithm Joystick

---
+++ Algorithm/Programmers/Level2/조이스틱/1.solution.js
@@ -0,0 +1,25 @@
+function solution(name) {
+	let maxALength = 0;
+
+	const changeAlphabet = letter => letter > 78 ? 91 - letter : letter - 65;
+	const findConsecutiveA = (str, i) => {
+		let cnt;
+		for (cnt = 0; cnt < str.length; cnt++) {
+			if (str[cnt] !== 'A') break;
+		}
+		return cnt - (i - 1) > maxALength ? cnt - (i - 1) : maxALength;
+	}
+
+	const totalMove = [...name].reduce((totalMove, letter, i) => {
+			if (letter !== 'A') return totalMove + changeAlphabet(name.charCodeAt(i)) + 1;
+
+			if (i === 0 || name[i -1] !== 'A') {
+				maxALength = findConsecutiveA(name.slice(i), i);
+			}
+			return totalMove + 1;
+		}, 0);
+
+	return totalMove - maxALength - 1;
+}
+
+console.log(solution("JEROEN"));
